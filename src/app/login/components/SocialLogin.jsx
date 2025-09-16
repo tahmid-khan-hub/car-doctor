64e6670ae50142f47fb24cc50dc14dbd192db717
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-16 22:18:00 +0600

    added social login in register page

--- src/app/login/components/SocialLogin.jsx
+++ src/app/login/components/SocialLogin.jsx
@@ -1,10 +1,19 @@
+"use client"
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa6";
 
 const SocialLogin = () => {
+  const router = useRouter();
   const handleSocialLogin = async(providerName) => {
-    const result =  await signIn(providerName, {redirect: false})
+    const result =  await signIn(providerName);
+    if(result?.ok){
+        router.push("/");
+        alert("Successfully logged in!");
+    }else{
+        alert("Something went wrong");
+    }
   };
   return (
     <div className="flex gap-4">

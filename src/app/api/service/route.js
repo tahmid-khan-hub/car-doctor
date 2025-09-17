827efb6afdcdf2ae16a1656fe79b25a947501ed6
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-17 18:35:48 +0600

    added a get api in service route

--- src/app/api/service/route.js
+++ src/app/api/service/route.js
@@ -1,4 +1,6 @@
+import { authOptions } from "@/lib/authOptions";
 import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
@@ -8,3 +10,15 @@ export const POST = async (req) => {
 
   return NextResponse.json(res);
 };
+
+export const GET = async(req) => {
+    const session = await getServerSession(authOptions);
+    if(session){
+        const email = session?.user?.email;
+        const BookedOrderCollection = dbConnect("bookings");
+        const res = await BookedOrderCollection.find({email}).toArray();
+        return NextResponse.json(res);
+    }
+
+    return NextResponse.json({});
+}

b4333f4fe33b54958f56fd5a549a75b2b73a13e7
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-18 20:16:59 +0600

    added a PATCH api for updatedBookingForm

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -9,3 +9,18 @@ export const GET = async(req, {params}) => {
     const singleBooked = await BookedOrderCollection.findOne({_id: new ObjectId(p.id)});
     return NextResponse.json(singleBooked);
 }
+
+export const PATCH = async(req, {params}) => {
+    const p = await params;
+    const BookedOrderCollection = dbConnect("bookings");
+    const query = {_id: new ObjectId(p.id)};
+
+    const body = await req.json();
+    const filter = {
+        $set: {...body}
+    }
+    const options = {upsert: true}
+    const updatedRes = await BookedOrderCollection.updateOne(query, filter, options)
+
+    return NextResponse(updatedRes);
+}

b62769513c8206d9961a6e5690a6a4664798416b
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-09-19 15:38:26 +0600

    added user validation in my-bookings route and added headers in my-bookings page

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,14 +1,26 @@
+import { authOptions } from "@/lib/authOptions";
 import dbConnect from "@/lib/dbConnect"
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async(req, {params}) => {
     const p = await params;
     const BookedOrderCollection = dbConnect("bookings");
+    const query = {_id: new ObjectId(p.id)};
 
-    const singleBooked = await BookedOrderCollection.findOne({_id: new ObjectId(p.id)});
-    return NextResponse.json(singleBooked);
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    const singleBooked = await BookedOrderCollection.findOne(query);
+    const isValidOwner = email === singleBooked?.email;
+
+    if(isValidOwner){
+        return NextResponse.json(singleBooked);
+    }
+    else{
+        return NextResponse.json({message: "forbidden action"}, {status: 403});
+    }
 }
 
 export const PATCH = async(req, {params}) => {
@@ -16,13 +28,23 @@ export const PATCH = async(req, {params}) => {
     const BookedOrderCollection = dbConnect("bookings");
     const query = {_id: new ObjectId(p.id)};
 
-    const body = await req.json();
-    const filter = {
-        $set: {...body}
-    }
-    const options = {upsert: true}
-    const updatedRes = await BookedOrderCollection.updateOne(query, filter, options)
+    const session = await getServerSession(authOptions);
+    const email = session?.user?.email;
+    const currBookedData = await BookedOrderCollection.findOne(query);
+    const isValidOwner = email === currBookedData?.email;
 
-    revalidatePath("/my-bookings")
-    return NextResponse.json(updatedRes);
+    if(isValidOwner){
+        const body = await req.json();
+        const filter = {
+            $set: {...body}
+        }
+        const options = {upsert: true}
+        const updatedRes = await BookedOrderCollection.updateOne(query, filter, options)
+
+        revalidatePath("/my-bookings")
+        return NextResponse.json(updatedRes);
+    }
+    else{
+        return NextResponse.json({message: "forbidden action"}, {status: 403});
+    }
 }
--- src/app/my-bookings/[id]/page.jsx
+++ src/app/my-bookings/[id]/page.jsx
@@ -1,9 +1,12 @@
 import BookingUpdateForm from "@/components/BookingUpdateForm";
+import { headers } from "next/headers";
 import React from "react";
 
 export default async function UpdatedBookingPage({params}) {
   const p = await params;
-  const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`);
+  const res = await fetch(`http://localhost:3000/api/my-bookings/${p.id}`, {
+    headers: await headers(),
+  });
   const data = await res.json();
   return (
     <div>
